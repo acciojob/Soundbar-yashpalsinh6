c8aeb6b7a76627f84e3f333f84d593468f5a04de
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-01 16:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-//your JS code here. If required.
-const buttons = document.querySelectorAll('.btn');
-const stopButton = document.querySelector('.stop');
+const audioPlayer = document.getElementById('audioPlayer');
 
-const sounds = {
-  applause: new Audio('sounds/applause.mp3'),
-  boo: new Audio('sounds/boo.mp3'),
-  gasp: new Audio('sounds/gasp.mp3'),
-  tada: new Audio('sounds/tada.mp3'),
-  victory: new Audio('sounds/victory.mp3'),
-  wrong: new Audio('sounds/wrong.mp3')
-};
+function playSound(soundFile) {
+  audioPlayer.src = `sounds/${soundFile}`;
+  audioPlayer.play();
+}
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const sound = sounds[button.innerText];
-    sound.play();
-  });
-});
-
-stopButton.addEventListener('click', () => {
-  for (let sound in sounds) {
-    sounds[sound].pause();
-    sounds[sound].currentTime = 0;
-  }
-});
+function stopSound() {
+  audioPlayer.pause();
+  audioPlayer.currentTime = 0;
+}
