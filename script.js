7de02de740dac43e88a1cf486f6d9fda7635cabc
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-01 15:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+    var audio = new Audio('sounds/' + soundFile);
+    audio.play();
+}
+
+function stopSound() {
+    var audioElements = document.getElementsByTagName('button');
+    for (var i = 0; i < audioElements.length; i++) {
+        audioElements[i].pause();
+        audioElements[i].currentTime = 0;
+    }
+}
